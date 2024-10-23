47e666dc8a4e15d8fc1419e93c7415c9f9aa8be6
Andrei <andreirutty.ar20@outlook.com>
2024-10-23 11:09:08 +0400

    Added Express JS

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-const http = require("http");
+const express = require("express");
 
-const server = http.createServer((req, res) => {});
+const app = express();
 
-const port = 3000;
-
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.listen(3000, () => {
+  console.log(`Server running on port 3000`)
+})
